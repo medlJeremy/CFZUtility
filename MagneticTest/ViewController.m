0d86528e38180bcdf3eac6879bd517090acb2a01
Jeremy Hovanec <jeremy@medlmobile.com>
2017-07-05 10:42:43 -0700

    tracking range of two beacons and recording 100 values for each

--- MagneticTest/ViewController.m
+++ MagneticTest/ViewController.m
@@ -29,6 +29,8 @@
     __weak IBOutlet UILabel *passangerBeaconLabel;
     __weak IBOutlet UIButton *stopBtn;
     __weak IBOutlet UIButton *startBtn;
+    __weak IBOutlet UILabel *driverReadingCountLabel;
+    __weak IBOutlet UILabel *passangerReadingCountLabel;
 }
 @end
 
@@ -67,6 +69,7 @@ int getUptimeInMilliseconds()
     // Do any additional setup after loading the view, typically from a nib.
 }
 - (IBAction)startTapped:(id)sender {
+    NSLog(@"Start pressed");
     _startTime = getUptimeInMilliseconds();
     
     // Regardless of whether the device is a transmitter or receiver, we need a beacon region.
@@ -91,23 +94,46 @@ int getUptimeInMilliseconds()
     passangerBeaconRegion.notifyEntryStateOnDisplay = YES;
     
     locationManagerDriver = [[CLLocationManager alloc] init];
+    if([locationManagerDriver respondsToSelector:@selector(requestWhenInUseAuthorization)])
+    {
+        [locationManagerDriver requestAlwaysAuthorization];
+    }
     [locationManagerDriver startMonitoringForRegion:driverBeaconRegion];
     [locationManagerDriver startRangingBeaconsInRegion:driverBeaconRegion];
     
     locationManagerPassanger = [[CLLocationManager alloc] init];
+    
+    
+    if([locationManagerPassanger respondsToSelector:@selector(requestWhenInUseAuthorization)])
+    {
+        [locationManagerPassanger requestAlwaysAuthorization];
+    }
     [locationManagerPassanger startMonitoringForRegion:passangerBeaconRegion];
     [locationManagerPassanger startRangingBeaconsInRegion:passangerBeaconRegion];
+    
+    locationManagerDriver.delegate = self;
+    locationManagerPassanger.delegate = self;
+    
+    
+    
+    
+    NSLog(@"%lu", (unsigned long)[[locationManagerDriver rangedRegions] count]);
 }
 
 - (IBAction)stopTapped:(id)sender {
-    driverSnapShots = 0;
-    passangerRange = 0;
-    _takeDriverSnapshot = true;
-    _takePassangerSnapshot = true;
+    NSLog(@"Stop Pressed");
+    if (_takePassangerSnapshot) {
+        _takeDriverSnapshot = false;
+        _takePassangerSnapshot = false;
+    } else {
+        driverSnapShots = 0;
+        passangerSnapShots = 0;
+        _takeDriverSnapshot = true;
+        _takePassangerSnapshot = true;
+    }
 }
 
 - (void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray<CLBeacon *> *)beacons inRegion:(CLBeaconRegion *)region {
-    
     for (CLBeacon *beacon in beacons) {
     
         if ([[beacon.proximityUUID UUIDString] compare: uuidDriver]) {
@@ -116,7 +142,18 @@ int getUptimeInMilliseconds()
             driverBeaconLabel.text = [NSString stringWithFormat:@"%ld", (long)driverRange];
             if (_takeDriverSnapshot) {
                 driverSnapShots += 1;
+                driverReadingCountLabel.text = [NSString stringWithFormat:@"%ld", driverSnapShots];
                 //add reading to printout
+                NSString *oldValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"DriverString"];
+                if (!oldValue) {
+                    NSString *valueToSave = [NSString stringWithFormat:@"%@\n%ld", uuidDriver, (long)driverRange];
+                    [[NSUserDefaults standardUserDefaults] setObject:valueToSave forKey:@"DriverString"];
+                    [[NSUserDefaults standardUserDefaults] synchronize];
+                } else {
+                    NSString *valueToSave = [oldValue stringByAppendingString:[NSString stringWithFormat:@"\n%ld", (long)driverRange]];
+                    [[NSUserDefaults standardUserDefaults] setObject:valueToSave forKey:@"DriverString"];
+                    [[NSUserDefaults standardUserDefaults] synchronize];
+                }
                 if (driverSnapShots == 100) {
                     _takeDriverSnapshot = false;
                 }
@@ -125,8 +162,19 @@ int getUptimeInMilliseconds()
             passangerRange = beacon.rssi;
             passangerBeaconLabel.text = [NSString stringWithFormat:@"%ld", (long)passangerRange];
             if (_takePassangerSnapshot) {
-                driverSnapShots += 1;
+                passangerSnapShots += 1;
+                passangerReadingCountLabel.text = [NSString stringWithFormat:@"%ld", passangerSnapShots];
                 //add reading to printout
+                NSString *oldValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"PassangerString"];
+                if (!oldValue) {
+                    NSString *valueToSave = [NSString stringWithFormat:@"%@\n%ld", uuidPassanger, (long)passangerRange];
+                    [[NSUserDefaults standardUserDefaults] setObject:valueToSave forKey:@"PassangerString"];
+                    [[NSUserDefaults standardUserDefaults] synchronize];
+                } else {
+                    NSString *valueToSave = [oldValue stringByAppendingString:[NSString stringWithFormat:@"\n%ld", (long)passangerRange]];
+                    [[NSUserDefaults standardUserDefaults] setObject:valueToSave forKey:@"PassangerString"];
+                    [[NSUserDefaults standardUserDefaults] synchronize];
+                }
                 if (passangerSnapShots == 100) {
                     _takePassangerSnapshot = false;
                 }
@@ -134,12 +182,26 @@ int getUptimeInMilliseconds()
         }
         
     }
+    
+    if (driverRange > passangerRange) {
+        driverBeaconLabel.textColor = [UIColor redColor];
+        passangerBeaconLabel.textColor = [UIColor blackColor];
+    } else {
+        passangerBeaconLabel.textColor = [UIColor redColor];
+        driverBeaconLabel.textColor = [UIColor blackColor];
+    }
 }
 
 - (IBAction)onPrintdriver:(UIButton *)sender {
+    NSLog(@"Driver Reading");
+    NSString *value = [[NSUserDefaults standardUserDefaults] stringForKey:@"DriverString"];
+    NSLog(@"%@", value);
 }
  
 - (IBAction)onPrintPassanger:(UIButton *)sender {
+    NSLog(@"Passanger Reading");
+    NSString *value = [[NSUserDefaults standardUserDefaults] stringForKey:@"PassangerString"];
+    NSLog(@"%@", value);
 }
 
 - (void)didReceiveMemoryWarning {
